661b645c79f4adb5f83b5b5a26f626a265e02d3b
Urban <upuhek@gmail.com>
2023-01-12 19:39:58 +0100

    simple product works

---
+++ assets/js/admin.js
@@ -0,0 +1,33 @@
+jQuery(function(){
+    jQuery('#the-list').on('click', '.editinline', function(){
+    
+        /**
+         * Extract metadata and put it as the value for the custom field form
+         */
+        inlineEditPost.revert();
+    
+        var post_id = jQuery(this).closest('tr').attr('id');
+    
+        post_id = post_id.replace("post-", "");
+    
+        var $cfd_inline_data = jQuery('#custom_field_demo_inline_' + post_id),
+            $wc_inline_data = jQuery('#woocommerce_inline_' + post_id );
+    
+        jQuery('input[name="_custom_field"]', '.inline-edit-row').val($cfd_inline_data.find("#_custom_field_demo").text());
+    
+        /**
+         * Only show custom field for appropriate types of products (simple)
+         */
+        var product_type = $wc_inline_data.find('.product_type').text();
+        
+        
+
+        if (product_type =='simple' || product_type=='variable') {
+            jQuery('.custom_field_demo', '.inline-edit-row').show();
+        } else {
+            jQuery('.custom_field_demo', '.inline-edit-row').css("display", "none");
+            jQuery('.onoff-bulk-discoun-br', '.inline-edit-row').css("display", "none");
+        }
+    
+    });
+});
--- onoff-bulk-discount.php
+++ onoff-bulk-discount.php
@@ -8,54 +8,50 @@
  * Author URI:        https://www.linkedin.com/in/urban-puhek-2227261b7/
  */
 
+ add_action('admin_enqueue_scripts', function (){
+	wp_enqueue_script('main-onoff-bulk-discount-js', plugin_dir_url( __FILE__ ) . 'assets/js/admin.js' , array('jquery'), '1.0');
+});
+
+
  add_action( 'woocommerce_product_quick_edit_start', function(){
 	global $post;
-	/*?>
-	<label>
+	?>
+	<label class="custom_field_demo">
 	   <span class="title">Custom field</span>
 	   <span class="input-text-wrap">
-		  <input id="onoff-discount-input" type="text" name="_custom_field" class="text" value="<?php echo esc_html( get_post_meta( $post->ID, '_custom_field', true ) );?>">
+		  <input type="text" name="_custom_field" class="text" value="">
 	   </span>
 	</label>
-	<br class="clear" />
-	<?php*/
-
-	woocommerce_wp_text_input( 
-		array( 
-			'id'          => '_custom_field', 
-			'label'       => __( 'Custom field', 'woocommerce' ),
-			'value'       => get_post_meta( $post->ID, '_custom_field', true ),		
-		)
-	);
+	<br class="clear onoff-bulk-discoun-br" />
+	<?php
  });
+
+ add_action( 'woocommerce_product_quick_edit_save', function ( $product ) {
+	$product_id = $product->get_id();
+
+	if ( isset( $_REQUEST['_custom_field'] ) ) {
+		$custom_field = $_REQUEST['_custom_field'];
+		update_post_meta( $product_id, '_custom_field', wc_clean( $custom_field) );
+
+		if($product->is_type('simple')){
+		   $current_product_price = $product->get_regular_price();
+		   $new_price = ((100-$custom_field)*$current_product_price)/100;
+		   $product->set_sale_price($new_price);
+		   $product->save();
+	   }else if($product->is_type('variable')){
+   
+	   }
+	}
+}, 10, 1);
   
  add_action( 'manage_product_posts_custom_column', function ( $column, $post_id ){
 	 if ( 'name' !== $column ) return;
-	 /*echo '<div>Custom field: <span id="cf_' . $post_id . '">' . esc_html( get_post_meta( $post_id, '_custom_field', true ) ) . '</span></div>';
-	wc_enqueue_js( "
-	   $('#the-list').on('click', '.editinline', function() {
-		  var post_id = $(this).closest('tr').attr('id');
-		  post_id = post_id.replace('post-', '');
-		  var custom_field = $('#cf_' + post_id).text();
-		  $('input[name=\'_custom_field\']', '.inline-edit-row').val(custom_field);
-		 });
-	" );*/
- });
+
+	?>
+	<div class="hidden custom_field_demo_inline" id="custom_field_demo_inline_<?php echo $post_id; ?>">
+		<div id="_custom_field_demo"><?php echo get_post_meta($post_id,'_custom_field',true); ?></div>
+ 	</div>
+
+	<?php
+ }, 99, 2);
   
- add_action( 'woocommerce_product_quick_edit_save', function ( $product ) {
-	 $product_id = $product->get_id();
-
-	 if ( isset( $_REQUEST['_custom_field'] ) ) {
-		 $custom_field = $_REQUEST['_custom_field'];
-		 update_post_meta( $product_id, '_custom_field', esc_attr( $custom_field) );
-
-		 if($product->get_type() == 'simple'){
-			$current_product_price = $product->get_regular_price();
-			$new_price = ((100-$custom_field)*$current_product_price)/100;
-			$product->set_sale_price($new_price);
-			$product->save();
-		}else if($product->get_type() == 'variable'){
-	
-		}
-	 }
- });
