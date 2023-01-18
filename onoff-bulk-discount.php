9673d1d0466b060fd93b3e21c885a79447805c31
Urban <urban@onoff.si>
2023-01-18 13:47:37 +0100

    experimenting

--- onoff-bulk-discount.php
+++ onoff-bulk-discount.php
@@ -20,7 +20,6 @@ add_action( 'woocommerce_product_quick_edit_start', 'onoff_bulk_discount_field_i
 
 
 function onoff_bulk_discount_field_input(){
-	global $post;
 	?>
 	<label class="onoff-bulk-discoun">
 	<span class="title">Discount (%)</span>
@@ -36,10 +35,11 @@ function onoff_bulk_discount_field_input(){
 add_action( 'woocommerce_product_bulk_edit_save', 'onoff_bulk_discount_field_save', 10, 1 );
 
 //Add action for Quick Edit
-add_action( 'woocommerce_product_quick_edit_save', 'onoff_bulk_discount_field_save' , 10, 2);
+//add_action( 'woocommerce_product_quick_edit_save', 'onoff_bulk_discount_field_save' , 10, 2);
 
 function onoff_bulk_discount_field_save( $product) {
-	//wp_die(isset( $_REQUEST['_discount_field'] ));
+	//echo '<script>alert("saved");</script>';
+	//wp_die(var_dump($_REQUEST['_discount_field'] ));
 	$product_id = $product->get_id();
 
 	if($_REQUEST['_discount_field'] != NULL){
@@ -91,3 +91,37 @@ add_action( 'manage_product_posts_custom_column', function ( $column, $post_id )
 <?php
 }, 99, 2);
   
+
+add_action( 'woocommerce_product_object_updated_props', 'onoff_bulk_discount_sale_updated', 1, 2);
+
+function onoff_bulk_discount_sale_updated($product, $updated_props){
+	//echo '<script>alert("updated");</script>';
+	//wp_die($product->get_sale_price());
+	wp_die(var_dump($updated_props));
+	if(in_array('sale_price', $updated_props, true)){
+		//wp_die('sale price');
+		//echo '<script>alert("updated");</script>';
+
+		$product_id = $product->get_id();
+		//wp_die($product_id);
+		$product_sale_price = $product->get_sale_price();
+		//wp_die($product_sale_price);
+		if($product_sale_price == NULL || $product_sale_price == 0){
+			//wp_die('tukaj sem');
+			//wp_die(get_post_meta($product_id,'_discount_field',true));
+			update_post_meta( $product_id, '_discount_field', 0 );
+			//wp_die(get_post_meta($product_id,'_discount_field',true));
+		}else{
+			$discount_field = get_post_meta($product_id, '_discount_field', true);
+			$product_regular_price = $product->get_regular_price();
+			$discount_field_price = ((100-$discount_field)*$product_regular_price)/100;
+
+			if($product_sale_price != $discount_field_price){
+				$new_discount_field = 100 - ((100*$product_sale_price)/$product_regular_price);
+				//wp_die($new_discount_field);
+				update_post_meta( $product_id, '_discount_field', wc_clean($new_discount_field));
+				//wp_die(get_post_meta($product_id,'_discount_field',true));
+			}
+		}
+	}
+}
